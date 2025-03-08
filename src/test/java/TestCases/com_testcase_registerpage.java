1ddf2024de104b0dcc792a65f06e812da2fabd47
atharavyadav <atharavyadav@gmail.com>
2025-03-08 08:50:54 +0530

    added logger file

--- src/test/java/TestCases/com_testcase_registerpage.java
+++ src/test/java/TestCases/com_testcase_registerpage.java
@@ -8,7 +8,8 @@ import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import driverIntializer.driverIntializer;
 import readDataFromExcel.excelReader;
 import readerUtils.readDataFromPeropertiesFile;
@@ -17,11 +18,13 @@ import seleniumActions.seleniumUIActions;
 
 public class com_testcase_registerpage {
 	
+	private static final Logger logger = LogManager.getLogger(com_testcase_registerpage.class);
 	
 	@BeforeTest
 	public void registerPageNavigation()
 	{
 		driverIntializer.launchdriver(resusebaleData.url);
+		logger.info("User is on the Register Page with URL Used is :=>"+resusebaleData.url);
 		
 	}
 
@@ -29,9 +32,14 @@ public class com_testcase_registerpage {
 	public void enterContactInformation() throws IOException
 	{
 
+		logger.info("***************This Steps included adding the data for ContactInformation***************");
+		logger.info("User enters Firstname and the Xpath is" + excelReader.readTestDataFromExcel(resusebaleData.excelpath, resusebaleData.sheetname_Contact, 1, 0));
 		seleniumUIActions.enterValueinUI(resusebaleData.regiterORPath, "Register.ContactInformation.FirstName.input", excelReader.readTestDataFromExcel(resusebaleData.excelpath, resusebaleData.sheetname_Contact, 1, 0));
+		logger.info("User enters lastname and the Xpath is" + excelReader.readTestDataFromExcel(resusebaleData.excelpath, resusebaleData.sheetname_Contact, 1, 1));
 		seleniumUIActions.enterValueinUI(resusebaleData.regiterORPath, "Register.ContactInformation.LastName.input", excelReader.readTestDataFromExcel(resusebaleData.excelpath, resusebaleData.sheetname_Contact, 1, 1));
+		logger.info("User enters phone and the Xpath is" + excelReader.readTestDataFromExcel(resusebaleData.excelpath, resusebaleData.sheetname_Contact, 1, 0));
 		seleniumUIActions.enterValueinUI(resusebaleData.regiterORPath, "Register.ContactInformation.phone.input", "1224");
+		logger.info("***************This Steps ended for ContactInformation***************");
 	}
 	
 	
